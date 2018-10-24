f500a7670f95ffe775f0bbd3d54db50f2106135a
dardomanski <dardomanski@gmail.com>
2018-10-24 11:21:52 +0200

    Added parameter to fund method to make it work with many crowdfund items

--- js/script.js
+++ js/script.js
@@ -22,21 +22,21 @@ function logIt(name) {
     console.log(name);
 }
 
-function fund() {
-    var actualPrice = document.getElementById("item_price").innerHTML;
+function fund(item_number) {
+    var actualPrice = document.getElementById("item_price" + item_number).innerHTML;
     var actualPriceInt = parseInt(actualPrice);
-    var contribution = parseInt(document.getElementById("fund_input").value);
+    var contribution = parseInt(document.getElementById("fund_input" + item_number).value);
 
     actualPriceInt = actualPriceInt - contribution;
 
     if (actualPriceInt < 0) {
-        document.getElementById("item_price").innerHTML = 0;
+        document.getElementById("item_price" + item_number).innerHTML = 0;
     } else {
-        document.getElementById("item_price").innerHTML = actualPriceInt;
+        document.getElementById("item_price" + item_number).innerHTML = actualPriceInt;
     }
 
-    if (document.getElementById("item_price").innerHTML == 0) {
-        document.getElementById("fund_button").disabled = true;
+    if (document.getElementById("item_price" + item_number).innerHTML == 0) {
+        document.getElementById("fund_button" + item_number).disabled = true;
     }
 
 }
