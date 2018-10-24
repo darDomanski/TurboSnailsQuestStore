07320cc6b3e7ca77904be8e02b091e6861d928df
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-10-24 19:54:42 +0200

    Implement adding new mentor to mentor list

--- js/mentors.js
+++ js/mentors.js
@@ -1,33 +1,124 @@
+// Collect all buttons and dialog box elements
 var editButton = document.getElementsByClassName("edit_button");
 var saveEditButton = document.getElementById("save_mentor_button");
 var exitEditButton = document.getElementById("exit_mentor_button");
-var addMentorButton = document.getElementById("add_mentor");
+var addMentorButton = document.getElementById("add_mentor_button");
+var addMentorOption = document.getElementById("add_mentor_option");
 
 var editWindow = document.getElementById("edit_window");
 var shadeBackground =  document.getElementById("shade_background");
 
-function showEditDialogBox() {
+// Show box for adding and editing mentor
+function showDialogBox(addOrEdit) {
+
     editWindow.style.display = "block";
     shadeBackground.style.display = "block";
+    if (addOrEdit == "addMentor") {
+        saveEditButton.style.display = "none";
+        addMentorButton.style.display = "block";
+    } else if (addOrEdit == "editMentor") {
+        addMentorButton.style.display = "none";
+        saveEditButton.style.display = "block";
+    }
 }
+
+// Clear all inputs and hide box
 function hideEditDialogBox() {
+    document.getElementById("edit_mentor_name").value = "";
+    document.getElementById("edit_mentor_class").value = "";
+    document.getElementById("edit_mentor_email").value = "";
     editWindow.style.display = "none";
     shadeBackground.style.display = "none";
 }
-var editMentor = function(){
-    showEditDialogBox();
+
+// Add function for all edit buttons
+var editMentor = function() {
+
+    var currentMentorName = document.getElementById()
+    showDialogBox("editMentor");
+};
+function addEventListeners() {
+    for (var i = 0; i < editButton.length; i++) {
+        editButton[i].addEventListener('click', editMentor, false);
+    };
+};
+addEventListeners();
+
+// Create new list item filled with new mentor info
+addMentorButton.addEventListener('click', function() {
+
+    var newMentorName = document.getElementById("edit_mentor_name").value;
+    var newMentorClass = document.getElementById("edit_mentor_class").value;
+    var newMentorEmail = document.getElementById("edit_mentor_email").value;
+
+    var newMentorListItem = document.createElement("li");
+
+    var mentorNameSpan = getMentorNameSpan(newMentorName);
+    newMentorListItem.appendChild(mentorNameSpan);
+    var mentorClassSpan = getMentorClassSpan(newMentorClass);    
+    newMentorListItem.appendChild(mentorClassSpan);
+    var mentorEmailSpan = getMentorEmailSpan(newMentorEmail);
+    newMentorListItem.appendChild(mentorEmailSpan);
+    var newMentorEditButton = getMentorEditButton();   
+    newMentorListItem.appendChild(newMentorEditButton);
+
+    document.getElementById("mentors").appendChild(newMentorListItem);
+
+    addEventListeners();
+    hideEditDialogBox();
+});
+
+// Get mentor name span from form input
+function getMentorNameSpan(newMentorName) {
+    var mentorNameSpan = document.createElement("span");
+    mentorNameSpan.classList.add("mentor_name");
+    var mentorNameNode = document.createTextNode(newMentorName);
+    mentorNameSpan.appendChild(mentorNameNode);
+
+    return mentorNameSpan;
 };
 
-for (var i = 0; i < editButton.length; i++) {
-    editButton[i].addEventListener('click', editMentor, false);
+// Get mentor class span from form input
+function getMentorClassSpan(newMentorClass) {
+    var mentorClassSpan = document.createElement("span");
+    mentorClassSpan.classList.add("mentor_class");
+    var mentorClassNode = document.createTextNode(newMentorClass);
+    mentorClassSpan.appendChild(mentorClassNode);
+
+    return mentorClassSpan;
 };
-saveEditButton.addEventListener('click', function(){
+
+// Get mentor e-mail span from form input
+function getMentorEmailSpan(newMentorEmail) {
+    var mentorEmailSpan = document.createElement("span");
+    mentorEmailSpan.classList.add("mentor_e_mail");
+    var mentorEmailNode = document.createTextNode(newMentorEmail);
+    mentorEmailSpan.appendChild(mentorEmailNode);
+
+    return mentorEmailSpan;
+};
+
+function getMentorEditButton() {
+    var newMentorEditButton = document.createElement("input");
+    newMentorEditButton.setAttribute("class", "edit_button");
+    newMentorEditButton.setAttribute("type", "button");
+    newMentorEditButton.setAttribute("name", "edit_mentor");
+    newMentorEditButton.setAttribute("value", "EDIT");
+
+    return newMentorEditButton;
+};
+
+// 
+saveEditButton.addEventListener('click', function() {
     hideEditDialogBox();
 });
-exitEditButton.addEventListener('click', function(){
+
+//
+exitEditButton.addEventListener('click', function() {
     hideEditDialogBox();
 });
-addMentorButton.addEventListener('click', function(){
-    showEditDialogBox();
-    
+
+//
+addMentorOption.addEventListener('click', function() {
+    showDialogBox("addMentor");    
 });
