82c1f81a0d153d6b77075e1632b696edb00bdbba
dardomanski <dardomanski@gmail.com>
2018-11-21 11:58:40 +0100

    created abstract Person class

--- back-end/src/main/java/com/codecool/quest_store/model/Person.java
+++ back-end/src/main/java/com/codecool/quest_store/model/Person.java
@@ -1,4 +1,67 @@
 package com.codecool.quest_store.model;
 
 public abstract class Person {
+    final int ID;
+    String firstName;
+    String lastName;
+    String email;
+    String className;
+    String userType;
+    String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
