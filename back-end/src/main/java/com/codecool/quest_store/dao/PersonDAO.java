627922290bbfef45ad7cf92b03aff407e8e7c28f
dardomanski <dardomanski@gmail.com>
2018-11-21 12:06:47 +0100

    created PersonDAO interface

--- back-end/src/main/java/com/codecool/quest_store/dao/PersonDAO.java
+++ back-end/src/main/java/com/codecool/quest_store/dao/PersonDAO.java
@@ -1,4 +1,8 @@
 package com.codecool.quest_store.dao;
 
 public interface PersonDAO {
+    List<Person> getAll();
+    void insert(Person person);
+    void update(int id, String column, String newValue);
+    void delete(int id);
 }
