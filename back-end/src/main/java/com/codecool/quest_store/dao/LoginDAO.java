f239e5185d6ffdf8bc3513d3e39b4e25d5acaead
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-21 17:09:06 +0100

    Implement LoginDAO interface and LoginDAOImpl class

--- back-end/src/main/java/com/codecool/quest_store/dao/LoginDAO.java
+++ back-end/src/main/java/com/codecool/quest_store/dao/LoginDAO.java
@@ -5,4 +5,8 @@ import com.codecool.quest_store.model.Person;
 public interface LoginDAO {
 
     Person getPersonByLoginPassword(String login, String password);
+    void addPerson(int id, String login, String password);
+    void updatePerson(int id, String value, String valueType);
+    void deletePerson(int id);
+
 }
