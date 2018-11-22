0cc84fec65ddc06d18e3e647ed0c951762573d22
dardomanski <dardomanski@gmail.com>
2018-11-22 11:15:49 +0100

    Delete unnecessary classes

--- back-end/src/main/java/com/codecool/quest_store/model/Student.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.quest_store.model;
-
-public class Student extends Person {
-    public Student(int ID, String firstName, String lastName, String email, String className, String userType, String status) {
-        super(ID, firstName, lastName, email, className, userType, status);
-    }
-}
