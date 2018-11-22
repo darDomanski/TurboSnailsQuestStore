13cc457f31f8cade40f732f2a481ffe2059c1017
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-22 11:19:06 +0100

    Implement createClass method

--- back-end/src/main/java/com/codecool/quest_store/dao/CreepyGuyDAO.java
+++ back-end/src/main/java/com/codecool/quest_store/dao/CreepyGuyDAO.java
@@ -2,26 +2,38 @@ package com.codecool.quest_store.dao;
 
 import com.codecool.quest_store.model.Person;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
-public class CreepyGuyDAO implements PersonDAO {
-    @Override
-    public List<Person> getAll() {
-        return null;
+
+public class CreepyGuyDAO {
+
+    public void createClass(String className) {
+
+        Connection connection = null;
+        PreparedStatement findUser = null;
+        String statement = "INSERT INTO class_ (name) VALUES (?)";
+
+        try {
+            connection = DBConnector.getConnection();
+            findUser = connection.prepareStatement(statement);
+            findUser.setString(1, className);
+            findUser.execute();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                findUser.close();
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
-    @Override
-    public void insert(Person person) {
-
-    }
-
-    @Override
-    public void update(int id, String column, String newValue) {
-
-    }
-
-    @Override
-    public void delete(int id) {
-
-    }
+    
 }
