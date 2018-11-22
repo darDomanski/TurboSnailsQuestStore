d9a603a06dac7417a27f8e9ddfefaee8257d294a
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-22 13:12:52 +0100

    Implement updateAccessLevels method

--- back-end/src/main/java/com/codecool/quest_store/dao/CreepyGuyDAO.java
+++ back-end/src/main/java/com/codecool/quest_store/dao/CreepyGuyDAO.java
@@ -10,24 +10,30 @@ import java.util.List;
 
 public class CreepyGuyDAO {
 
+    public static void main(String[] args) {
+        CreepyGuyDAO guyDao = new CreepyGuyDAO();
+        int[] update = new int[] {50, 51, 150, 151, 250, 251, 350, 351};
+        guyDao.updateAccessLevels(update);
+    }
+
     public void createClass(String className) {
 
         Connection connection = null;
-        PreparedStatement findUser = null;
+        PreparedStatement query = null;
         String statement = "INSERT INTO class_ (name) VALUES (?)";
 
         try {
             connection = DBConnector.getConnection();
-            findUser = connection.prepareStatement(statement);
-            findUser.setString(1, className);
-            findUser.execute();
+            query = connection.prepareStatement(statement);
+            query.setString(1, className);
+            query.execute();
 
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
                 connection.close();
-                findUser.close();
+                query.close();
             }
             catch (SQLException ex) {
                 ex.printStackTrace();
@@ -35,5 +41,43 @@ public class CreepyGuyDAO {
         }
     }
 
-    
+    public int[] getAccessLevels() {
+        return null;
+    }
+
+    public void updateAccessLevels(int[] update) {
+
+        Connection connection = null;
+        PreparedStatement query = null;
+        String statement = "UPDATE access_level SET max_lifetime_coins = ? WHERE level_id = 1; " +
+                "UPDATE access_level SET min_lifetime_coins = ?, max_lifetime_coins = ? WHERE level_id = 2; " +
+                "UPDATE access_level SET min_lifetime_coins = ?, max_lifetime_coins = ? WHERE level_id = 3; " +
+                "UPDATE access_level SET min_lifetime_coins = ?, max_lifetime_coins = ? WHERE level_id = 4; " +
+                "UPDATE access_level SET min_lifetime_coins = ? WHERE level_id = 5;";
+
+        try {
+            connection = DBConnector.getConnection();
+            query = connection.prepareStatement(statement);
+            query.setInt(1, update[0]);
+            query.setInt(2, update[1]);
+            query.setInt(3, update[2]);
+            query.setInt(4, update[3]);
+            query.setInt(5, update[4]);
+            query.setInt(6, update[5]);
+            query.setInt(7, update[6]);
+            query.setInt(8, update[7]);
+            query.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+                query.close();
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
 }
