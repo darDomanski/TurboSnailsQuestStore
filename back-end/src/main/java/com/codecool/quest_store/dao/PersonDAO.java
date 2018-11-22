6007a6746240c47c8c16b8407eaacd99479490f3
dardomanski <dardomanski@gmail.com>
2018-11-22 18:35:16 +0100

    Refactor code PersonDAO

--- back-end/src/main/java/com/codecool/quest_store/dao/PersonDAO.java
+++ back-end/src/main/java/com/codecool/quest_store/dao/PersonDAO.java
@@ -5,8 +5,8 @@ import com.codecool.quest_store.model.Person;
 import java.util.List;
 
 public interface PersonDAO {
-    List<Person> getAll(String userType);
 
+    List<Person> getAll(String userType);
     List<Person> getStudentsByClass(String className);
     void insert(Person person);
     void update(int id, String column, String newValue);
