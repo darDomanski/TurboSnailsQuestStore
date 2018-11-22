ae5f3d30dc09fcdfa34887f69e2fe74c61673c4c
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-11-22 15:01:38 +0100

    Remove unused imports in CreepyGuyDAO class

--- back-end/src/main/java/com/codecool/quest_store/dao/CreepyGuyDAO.java
+++ back-end/src/main/java/com/codecool/quest_store/dao/CreepyGuyDAO.java
@@ -1,13 +1,10 @@
 package com.codecool.quest_store.dao;
 
-import com.codecool.quest_store.model.Person;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 
