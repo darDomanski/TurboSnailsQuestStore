db0c28441bb3f38aa8e9388d462f553d29e6f03c
darDomanski <dardomanski@gmail.com>
2018-12-03 21:40:23 +0100

    Create LvelsDAO interface

--- src/main/java/com/codecool/quest_store/dao/LevelsDAO.java
+++ src/main/java/com/codecool/quest_store/dao/LevelsDAO.java
@@ -2,4 +2,5 @@ package com.codecool.quest_store.dao;
 
 public interface LevelsDAO {
     int getStudentLevel(int studentId);
+    void updateLevel(int levelId, int minCoolcoinsAmount, int maxCoolcoinsAmount);
 }
