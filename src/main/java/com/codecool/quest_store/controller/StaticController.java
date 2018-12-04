d99aa59c74dd2300181a2580a9ba920880b75409
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-12-04 09:34:45 +0100

    First attempts to load static files

--- src/main/java/com/codecool/quest_store/controller/StaticController.java
+++ src/main/java/com/codecool/quest_store/controller/StaticController.java
@@ -3,13 +3,17 @@ package com.codecool.quest_store.controller;
 import com.codecool.quest_store.controller.helpers.MimeTypeResolver;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.apache.commons.io.IOUtils;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
+import  java.nio.file.Files;
+
 import java.net.URI;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import static java.lang.System.in;
 
 
 public class StaticController implements HttpHandler {
@@ -24,8 +28,6 @@ public class StaticController implements HttpHandler {
         // get file from resources folder, see: https://www.mkyong.com/java/java-read-a-file-from-resources-folder/
         ClassLoader classLoader = getClass().getClassLoader();
         URL fileURL = classLoader.getResource(path);
-
-        OutputStream os = httpExchange.getResponseBody();
 
         if (fileURL == null) {
             // Object does not exist or is not a file: reject with 404 error.
@@ -57,14 +59,49 @@ public class StaticController implements HttpHandler {
 
         OutputStream os = httpExchange.getResponseBody();
 
+//        System.out.println(path);
+//
+//        String result = null;
+//        try {
+//            result = IOUtils.toString(classLoader.getResourceAsStream(path));
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        os.write(result.getBytes());
+//        os.close();
+
         // send the file
-        FileInputStream fs = new FileInputStream(file);
-        final byte[] buffer = new byte[0x10000];
-        int count = 0;
-        while ((count = fs.read(buffer)) >= 0) {
-            os.write(buffer,0,count);
+//        FileInputStream fs = new FileInputStream(file);
+//        final byte[] buffer = new byte[0x10000];
+//        int count = 0;
+//        while ((count = fs.read(buffer)) >= 0) {
+//            os.write(buffer,0,count);
+//        }
+//        os.close();
+
+
+        FileInputStream fis = new FileInputStream(file);
+        //System.out.println(file.exists() + "!!");
+        //InputStream in = resource.openStream();
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        byte[] buf = new byte[1024];
+        try {
+            for (int readNum; (readNum = fis.read(buf)) != -1;) {
+                bos.write(buf, 0, readNum); //no doubt here is 0
+                //Writes len bytes from the specified byte array starting at offset off to this byte array output stream.
+                System.out.println("read " + readNum + " bytes,");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
-        os.close();
+        byte[] bytes = bos.toByteArray();
+
+        //below is the different part
+        // File someFile = new File("java2.pdf");
+        FileOutputStream fos = new FileOutputStream(file);
+        fos.write(bytes);
+        fos.flush();
+        fos.close();
     }
 
 
