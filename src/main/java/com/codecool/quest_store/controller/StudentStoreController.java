12990a172fbd0aa204481835481066960db4c73a
Roman Bulinski <aboandrus@gmail.com>
2018-12-04 14:05:16 +0100

    Do conection student controler with quest DB

--- src/main/java/com/codecool/quest_store/controller/StudentStoreController.java
+++ src/main/java/com/codecool/quest_store/controller/StudentStoreController.java
@@ -1,15 +1,65 @@
 package com.codecool.quest_store.controller;
 
+import com.codecool.quest_store.dao.DBConnector;
+import com.codecool.quest_store.dao.ItemDAO;
+import com.codecool.quest_store.dao.QuestDAO;
+import com.codecool.quest_store.model.Item;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import org.jtwig.JtwigModel;
+import org.jtwig.JtwigTemplate;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
 
 
 public class StudentStoreController implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
+        String response = "";
+        String method = httpExchange.getRequestMethod();
 
+        // Probably should be in view
+        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/student/store_old.twig");
+        JtwigModel model = JtwigModel.newModel();
+
+        DBConnector dbConnector = new DBConnector();
+        ItemDAO items = new QuestDAO(dbConnector.getConnection());
+        List<Item> artifacts = items.getAll();
+
+//        System.out.println(artifacts.size());
+//        for ( int i=0; i < artifacts.size(); i++ ){
+//            System.out.println(artifacts.get(i).getTitle());
+//            System.out.println(artifacts.get(i).getValue());
+//
+//        }
+
+        // Send a form if it wasn't submitted yet.
+        if(method.equals("GET")){
+
+            model.with("artifacts", artifacts);
+            response = template.render(model);
+
+            httpExchange.sendResponseHeaders(200, 0);
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+
+        // If the form was submitted, retrieve it's content.
+        if(method.equals("POST")){
+
+            model.with("artifacts", artifacts);
+            response = template.render(model);
+
+            response = template.render(model);
+            httpExchange.sendResponseHeaders(200, 0);
+            OutputStream os = httpExchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
+
 }
