622a24d1a5bb4ee1c6d2f3e931c1d8682dbbf24f
darDomanski <dardomanski@gmail.com>
2018-12-04 14:23:33 +0100

    Implement POST request

--- src/main/java/com/codecool/quest_store/controller/LoginController.java
+++ src/main/java/com/codecool/quest_store/controller/LoginController.java
@@ -1,15 +1,32 @@
 package com.codecool.quest_store.controller;
 
+import com.codecool.quest_store.dao.LoginDAO;
+import com.codecool.quest_store.dao.LoginDAOImpl;
+import com.codecool.quest_store.model.Person;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.util.Map;
 
 
 public class LoginController implements HttpHandler {
+    private Connection connection;
+    private RedirectController redirectController;
+    private LoginDAO loginDAO;
+
+    public LoginController(Connection connection) {
+        this.connection = connection;
+        this.loginDAO = new LoginDAOImpl(this.connection);
+        this.redirectController = new RedirectController();
+    }
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
@@ -17,12 +34,12 @@ public class LoginController implements HttpHandler {
         String method = httpExchange.getRequestMethod();
 
         // Probably should be in view
-        JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/login.twig");;
-        JtwigModel model = JtwigModel.newModel();
 
         // Send a form if it wasn't submitted yet.
         if(method.equals("GET")){
 
+            JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/login.twig");
+            JtwigModel model = JtwigModel.newModel();
             response = template.render(model);
             httpExchange.sendResponseHeaders(200, 0);
             OutputStream os = httpExchange.getResponseBody();
@@ -31,13 +48,31 @@ public class LoginController implements HttpHandler {
         }
 
         // If the form was submitted, retrieve it's content.
-        if(method.equals("POST")){
+        if (method.equals("POST")) {
+            String context;
+            InputStreamReader isr = new InputStreamReader(httpExchange.getRequestBody(), StandardCharsets.UTF_8);
+            BufferedReader br = new BufferedReader(isr);
+            String formData = br.readLine();
+            Map inputs = redirectController.parseFormData(formData);
+            String username = (String) inputs.get("login");
+            String password = (String) inputs.get("password");
 
-            response = template.render(model);
-            httpExchange.sendResponseHeaders(200, 0);
-            OutputStream os = httpExchange.getResponseBody();
-            os.write(response.getBytes());
-            os.close();
+            Person person = loginDAO.getPersonByLoginPassword(username, password);
+
+            if (person != null) {
+                String userType = loginDAO.getUserTypeById(person.getId());
+                if (userType.equals("mentor")) {
+                    context = "mentor/codecoolers";
+                } else if (userType.equals("student")) {
+                    context = "student/store";
+                } else {
+                    context = "creepyguy/classes";
+                }
+            } else {
+                context = "login";
+            }
+            redirectController.redirect(httpExchange, context);
+
         }
     }
 }
