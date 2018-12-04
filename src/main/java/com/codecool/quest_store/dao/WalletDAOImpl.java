e0ee6bf7f4e54e7739ecaaf7b70db705a81a7abc
KrzysiekJodlowski <krzysztof.s.jodlowski@gmail.com>
2018-12-04 14:53:18 +0100

    Implement connection pool handling in WalletDAOimpl

--- src/main/java/com/codecool/quest_store/dao/WalletDAOImpl.java
+++ src/main/java/com/codecool/quest_store/dao/WalletDAOImpl.java
@@ -17,10 +17,12 @@ public class WalletDAOImpl implements WalletDAO {
         int coolcoinsAmount = 0;
         String query = "SELECT * FROM wallet WHERE student_id = ?;";
 
+        Connection connection = null;
         PreparedStatement preparedStatement;
         ResultSet resultSet;
 
         try {
+            connection = connectionPool.getConnection();
             preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, studentId);
             resultSet = preparedStatement.executeQuery();
@@ -28,9 +30,9 @@ public class WalletDAOImpl implements WalletDAO {
             while (resultSet.next()) {
                 coolcoinsAmount = resultSet.getInt(columnName);
             }
-
             preparedStatement.close();
             resultSet.close();
+            connection.close();
 
         } catch (SQLException e) {
             System.err.println("Can't get students coolcoins amount!");
@@ -43,15 +45,18 @@ public class WalletDAOImpl implements WalletDAO {
     public void changeCoolcoinsAmount(int coolcoinsAmount, String columnName, int studentId) {
         String query = String.format("UPDATE wallet SET %s=? WHERE student_id=?;", columnName);
 
+        Connection connection = null;
         PreparedStatement preparedStatement;
 
         try {
+            connection = connectionPool.getConnection();
             preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, coolcoinsAmount);
             preparedStatement.setInt(2, studentId);
             preparedStatement.executeUpdate();
 
             preparedStatement.close();
+            connection.close();
         } catch (SQLException e) {
             System.err.println("Can't change students coolcoin amount!");
             e.printStackTrace();
