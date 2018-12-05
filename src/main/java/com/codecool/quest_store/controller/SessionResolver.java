19181eb8e7eae53b25cb8fe7857993adce69ee33
darDomanski <dardomanski@gmail.com>
2018-12-05 12:02:18 +0100

    Implement SessionResolver

---
+++ src/main/java/com/codecool/quest_store/controller/SessionResolver.java
@@ -0,0 +1,49 @@
+package com.codecool.quest_store.controller;
+
+import com.codecool.quest_store.dao.*;
+import com.codecool.quest_store.model.Person;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.net.HttpCookie;
+
+public class SessionResolver {
+    private HttpExchange httpExchange;
+    private RedirectController redirectController;
+    private DBConnector connectionPool;
+    private SessionDAO sessionDAO;
+    private PersonDAO personDAO;
+
+    public SessionResolver(HttpExchange httpExchange, DBConnector connectionPool) {
+        this.httpExchange = httpExchange;
+        this.redirectController = new RedirectController();
+        this.connectionPool = connectionPool;
+        this.sessionDAO = new SessionDAOImpl(connectionPool);
+        this.personDAO = new QSUserDAO(connectionPool);
+    }
+
+    public void checkIfSessionIsValid() throws IOException {
+        String cookieStr = httpExchange.getRequestHeaders().getFirst("Cookie");
+
+        if (cookieStr == null) {
+            redirectController.redirect(httpExchange, "login");
+        } else {
+            HttpCookie cookie = HttpCookie.parse(cookieStr).get(0);
+            int userId = sessionDAO.getUserIdBySession(cookie.getValue());
+            String requestURI = httpExchange.getRequestURI().toString();
+            System.out.println(requestURI + "     uRI");
+
+            if (userId == 0) {
+                if (!requestURI.contains("creepyguy")) {
+                    redirectController.redirect(httpExchange, "login");
+                }
+            } else {
+                Person user = personDAO.getPersonById(userId);
+                if (!requestURI.contains(user.getUserType())) {
+                    redirectController.redirect(httpExchange, "login");
+                }
+            }
+        }
+
+    }
+}
